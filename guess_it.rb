2da40da828bb031921f24176b952f34ffb2c50aa
miguelmassv <miguelmassv@hotmail.com>
2018-05-17 20:25:40 -0500

    Plantilla proyecto

---
+++ guess_it.rb
@@ -0,0 +1,16 @@
+# 1. Leer el archivo con Q-A
+#   Preguntar por nombre y extencion del archivo
+#   Guardar Q-A
+#   Separar Q-A en un array de hash
+#   Agregar key de solucionado: true false
+#  
+# 2. Realizar preguntas al usuario
+#   Escoger primer hash del arreglo que no este solucionado
+#   Realizar la pregunta del Hash seleccionado 
+# 3. Validar respuesta
+#   si la respuesta es correcta cambiar estado de solucionado a true
+#   sino volver a preguntar hasta responder correctamente
+# 4. Siguiente pregunta
+#    cuando todas las preguntas este respuestas salir del juego
+
+
